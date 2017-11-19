8782d6b83c3ce4774b0b6b64c65dede254afac61
Nanrou <kkkcomkkk@qq.com>
2017-11-19 21:44:53 +0800

    update handle

--- maoxiaolu/app.py
+++ maoxiaolu/app.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import logging
+import json
+from datetime import timedelta
+from random import sample
 from logging import handlers
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
@@ -9,7 +12,7 @@ logger = logging.getLogger('wx')
 logger.setLevel(logging.DEBUG)
 
 handler = handlers.RotatingFileHandler(
-    os.path.join(BASEPATH, 'wx'),
+    'wx',
     maxBytes=10,
     backupCount=3,
 )
@@ -17,10 +20,14 @@ logger.addHandler(handler)
 
 sys.path.append(os.path.abspath('.'))
 
+from redis import Redis
 from werobot import WeRoBot
 from werobot.client import Client
 from werobot.config import Config
 from werobot.logger import enable_pretty_logging
+from werobot.replies import ImageReply
+
+from tuling import talk_api
 
 enable_pretty_logging(logger, 'debug')
 
@@ -30,14 +37,45 @@ dev = Config(
     HOST="127.0.0.1",
     PORT="8888",
     SESSION_STORAGE=None,
-    APP_ID='1234567',
-    APP_SECRET='1234567',
-    #    ENCODING_AES_KEY = 'lulu'
+    APP_ID='wx75358df345194f95',
+    APP_SECRET='5cb7f57f79672901572e8832977cb469',
+    ENCODING_AES_KEY='GSNR3TnAcalECiQXixBTwrTmO3qdHTXRfQH8XiFcLQj'
+
 )
 
 cc = dev
 robot = WeRoBot(config=cc)
 local_client = Client(config=cc)
+redis_db = Redis(db=2)
+
+
+@robot.subscribe
+def say_hi(message):
+    return '谢谢你关注毛小露呀~\n回复 我要看猫 就可以看猫啦'
+
+
+@robot.text
+def handle_text(message):
+    if message == '我要看猫':
+        if redis_db.exists('cat:imgs'):
+            imgs = redis_db.smembers('cat:imgs')
+        else:
+            tmp_json = local_client.get_media_list('image', 0, 20)
+            tmp = json.loads(tmp_json)
+            tmp_imgs = tmp.get('items')
+            imgs = []
+            for item in tmp_imgs:
+                imgs.append(item.get('media_id'))
+            redis_db.sadd('cat:imgs', imgs)
+        redis_db.expire('cat:imgs', timedelta(days=1))
+        if len(imgs) > 0:
+            that_img = sample(imgs, 1)[0]
+            return ImageReply(media_id=that_img)
+        else:
+            return '猫猫都出去了'
+    else:
+        return talk_api(message)
+
 
 if __name__ == '__main__':
     robot.run()
