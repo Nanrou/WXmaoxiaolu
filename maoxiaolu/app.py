635e8d31b98a33c4f6fb157828cf68211cd4da8a
Nanrou <kkkcomkkk@qq.com>
2017-11-19 20:20:59 +0800

    update config

--- maoxiaolu/app.py
+++ maoxiaolu/app.py
@@ -5,23 +5,22 @@ sys.path.append(os.path.abspath('.'))
 
 from werobot import WeRoBot
 from werobot.client import Client
+from werobot.config import Config
 
+dev = Config(
+    TOKEN='luluaiamao',
+    SERVER="auto",
+    HOST="127.0.0.1",
+    PORT="8888",
+    SESSION_STORAGE=None,
+    APP_ID='1234567',
+    APP_SECRET='1234567',
+    #    ENCODING_AES_KEY = 'lulu'
+)
 
-class DevConfig:
-    TOKEN = None,
-    SERVER = "auto",
-    HOST = "127.0.0.1",
-    PORT = "8888",
-    SESSION_STORAGE = None,
-    APP_ID = '1234567',
-    APP_SECRET = '1234567',
-    ENCODING_AES_KEY = 'lulu'
-
-
-cc = DevConfig
-robot = WeRoBot(cc)
-local_client = Client(cc)
-
+cc = dev
+robot = WeRoBot(config=cc)
+local_client = Client(config=cc)
 
 if __name__ == '__main__':
     robot.run()
